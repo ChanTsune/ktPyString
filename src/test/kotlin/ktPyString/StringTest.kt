6c5ed9d28df69dc439b1873e81d6b605b1e13313
taiki tsunekawa <s1f101702431@iniad.org>
2019-09-17 11:21:05 +0900

    [update] tetst

--- src/test/kotlin/ktPyString/StringTest.kt
+++ src/test/kotlin/ktPyString/StringTest.kt
@@ -6,18 +6,23 @@ import kotlin.test.*
 class StringTest {
     @Test fun testMultiplication() {
         val str = "Hello World!"
-        assertEquals(str * 2,"Hello World!Hello World!")
+        assertEquals("Hello World!Hello World!",str * 2)
     }
     @Test fun testSlice() {
-        assertEquals("abc"[1,2],"b")
+        val b = "Hello, world"
+        assertEquals("Hello", b[null,5])
+        assertEquals("ello", b[1,5])
+        assertEquals(", ", b[5,7])
+        assertEquals("world",b[7,null])
+        
     }
     @Test fun testCenter() {
         val str = "12".center(5)
-        assertEquals(str.length,5)
-        assertEquals(str,"  12 ")
+        assertEquals(5,str.length)
+        assertEquals("  12 ",str)
     }
     @Test fun testCount() {
-        assertEquals("aaaaa".count("a"),5)
+        assertEquals(5,"aaaaa".count("a"))
     }
     @Test fun testEndswith() {
         assertTrue("1234567890".endswith("0"))
@@ -25,17 +30,24 @@ class StringTest {
         assertFalse("1234567890".endswith("9"))
     }
     @Test fun testExpandtabs() {
-        assertEquals("\t".expandtabs(),"        ")
-        assertEquals("\t".expandtabs(4),"    ")
+        assertEquals("        ","\t".expandtabs())
+        assertEquals("    ","\t".expandtabs(4))
     }
     @Test fun testFind() {
-        assertEquals("01234567890".find("2"),2)
-        assertEquals("01234567890".find("45"),3)
-        assertEquals("0123456789".find(""),-1)
+        assertEquals(2,"01234567890".find("2"))
+        assertEquals(3,"01234567890".find("45"))
+        assertEquals(-1,"0123456789".find(""))
     }
     @Test fun testIndex() {
-        assertEquals("0123456789".find("2"),2)
-        assertEquals("01234567890".find("45"),3)
-        assertEquals("0123456789".find(""),-1)
+        assertEquals(2,"0123456789".find("2"))
+        assertEquals(3,"01234567890".find("45"))
+        assertEquals(-1,"0123456789".find(""))
+    }
+    @Test fun testJoin() {
+        assertEquals("1,2,3",",".join(listOf("1","2","3")))
+    }
+    @Test fun testLjust() {
+        assertEquals("ab   ","ab".ljust(5))
+        assertEquals("ab000","ab".ljust(5))
     }
 }
