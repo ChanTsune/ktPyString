debd6220341e6ff674c0a2b877325e90b8bae237
ChanTsune <Kizu199980425@yahoo.co.jp>
2019-12-26 18:02:18 +0900

    [update] `Slice` class tests

--- src/test/kotlin/ktPyString/SliceTest.kt
+++ src/test/kotlin/ktPyString/SliceTest.kt
@@ -1,23 +1,31 @@
 package ktPyString
 
 import ktPyString.utils.Quad
-import kotlin.test.Test
-import kotlin.test.assertEquals
+import kotlin.test.*
 
-class SliceTest {
-    @Test fun testSliceInit() {
-        val sliceObj = Slice(1)
-        assertEquals(sliceObj, sliceObj)
+internal class SliceTest {
+
+    private val slice = Slice(1)
+
+    @Test
+    fun testToString() {
+        assertEquals("Slice(null, 1, null)", slice.toString())
     }
-    @Test fun testSliceInit2() {
+
+    @Test
+    fun testSliceInit2() {
         val sliceObj = Slice(1,2)
         assertEquals(sliceObj, sliceObj)
     }
-    @Test fun testSliceInit3() {
+
+    @Test
+    fun testSliceInit3() {
         val sliceObj = Slice(null, step=2)
         assertEquals(sliceObj, sliceObj)
     }
-    @Test fun testSliceAdjustIndex() {
+
+    @Test
+    fun testSliceAdjustIndex() {
         val sliceObj = Slice(null,null,1)
         assertEquals(sliceObj.adjustIndex(20), Quad(0,20,1,20))
         val sliceObj2 = Slice(null,4,2)
