9085ca15d1f8a76ee4838d574612bb0ec3ef415b
ChanTsune <Kizu19980425@yahoo.co.jp>
2020-02-14 00:00:43 +0900

    [add] numericType

--- src/test/kotlin/ktPyString/properties/NumericTypeTest.kt
+++ src/test/kotlin/ktPyString/properties/NumericTypeTest.kt
@@ -6,7 +6,19 @@ import kotlin.test.assertEquals
 
 internal class NumericTypeTest {
     @Test
-    fun init(){
-        assertEquals(NumericType.NOT_NUMERIC,' '.numericType)
+    fun notNumeric(){
+        assertEquals(NumericType.NOT_NUMERIC, ' '.numericType)
+    }
+    @Test
+    fun decimal(){
+        assertEquals(NumericType.DECIMAL, '1'.numericType)
+    }
+    @Test
+    fun digit(){
+        assertEquals(NumericType.DIGIT, '①'.numericType)
+    }
+    @Test
+    fun numeric(){
+        assertEquals(NumericType.NUMERIC, '¾'.numericType)
     }
 }
