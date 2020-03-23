94688b8d9a6648ff45b8fe2a2f307b579aced1a9
ChanTsune <s1f101702431@iniad.org>
2020-03-23 15:31:48 +0900

    Fix expandtabs
    Conforms to Python behavior.

--- src/main/kotlin/ktPyString/ktPyString.kt
+++ src/main/kotlin/ktPyString/ktPyString.kt
@@ -51,7 +51,38 @@ fun String.count(sub: String, start: Int? = null, end: Int? = null): Int {
 fun String.endswith(suffix: String, start: Int? = null, end: Int? = null): Boolean =
     this[Slice(start, end)].endsWith(suffix)
 
-fun String.expandtabs(tabsize: Int = 8): String = this.replace("\t", " " * tabsize)
+/**
+ * Return a copy of the string where all tab characters are replaced by one or more spaces,
+ * depending on the current column and the given tab size.
+ * Tab positions occur every [tabsize] characters (default is 8, giving tab positions at columns 0, 8, 16 and so on).
+ * To expand the string, the current column is set to zero and the string is examined character by character.
+ * If the character is a tab (\t),
+ * one or more space characters are inserted in the result until the current column is equal to the next tab position.
+ * (The tab character itself is not copied.)
+ * If the character is a newline (\n) or return (\r), it is copied and the current column is reset to zero.
+ * Any other character is copied unchanged and the current column is incremented by one regardless of how the character is represented when printed.
+ * @param tabsize Size of tab('\t').
+ */
+fun String.expandtabs(tabsize: Int = 8): String {
+    var result = ""
+    var linePos = 0
+    for (ch in this) {
+        if (ch == '\t') {
+            if (tabsize > 0) {
+                val incr = tabsize - (linePos % tabsize)
+                linePos += incr
+                result += " " * incr
+            }
+        }
+        else {
+            linePos++
+            result += ch
+            if (ch == '\n' || ch == '\r')
+                linePos = 0
+        }
+    }
+    return result
+}
 
 
 fun String.find(sub: String, start: Int? = null, end: Int? = null): Int {
